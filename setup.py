41563c2388e8847bba5f016d2239970ee2210878
hd <mofo>
2017-07-30 02:13:53 -0400

    Worked on setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'flask_render_specific_template',
+  packages = ['flask_render_specific_template'],
+  version = '1.0',
+  description = 'This library allows Flask developers to dynamically select the template directory used for rendering',
+  author = 'Herbert Dawkins',
+  author_email = 'DrDawkins@ClearScienceInc.com',
+  url = 'https://github.com/sudouser2010/flask_render_specific_template',
+  download_url = 'https://github.com/sudouser2010/flask_render_specific_template/archive/1.0.tar.gz',
+  keywords = ['flask', 'dynamic', 'specific', 'template'],
+  classifiers = [],
+)
