f1d56a5ff60c12a6ecc3708a9094f58bffab2efc
herbert <hd>
2017-11-12 22:47:45 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'flask_render_specific_template',
   packages = ['flask_render_specific_template'],
-  version = '1.0',
+  version = '1.1',
   description = 'This library allows Flask developers to dynamically select the template directory used for rendering',
   author = 'Herbert Dawkins',
   author_email = 'DrDawkins@ClearScienceInc.com',
@@ -10,4 +10,7 @@ setup(
   download_url = 'https://github.com/sudouser2010/flask_render_specific_template/archive/1.0.tar.gz',
   keywords = ['flask', 'dynamic', 'specific', 'template'],
   classifiers = [],
+  install_requires=[
+  'flask==0.12.2',
+  ],
 )
